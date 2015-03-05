3717bbcdd743957eaa9ab27eb0449965caf9d34d
Doug Cox <dougcoxs1@gmail.com>
2015-03-05 11:29:46 -0800

    Also could not use put as it uses sftp. This is a little dirty, as it will ask for the password twice.

--- tools/fabfile.py
+++ tools/fabfile.py
@@ -7,7 +7,10 @@ def upload_ca():
     # make sure /etc/ssl/certs exists
     run('mkdir -p /etc/ssl/certs')
     # install the root ca
-    put('DigiCertHighAssuranceEVRootCA.pem', '/etc/ssl/certs')
+    # cannot use 'put' as sftp is not available
+    #put('DigiCertHighAssuranceEVRootCA.pem', '/etc/ssl/certs')
+    for host in env.hosts:
+        local('scp DigiCertHighAssuranceEVRootCA.pem root@{0}:/etc/ssl/certs/'.format(host))
     # create the symbolic links normally created by OpenSSL's c_rehash
     with cd('/etc/ssl/certs'):
         run('ln -s DigiCertHighAssuranceEVRootCA.pem 244b5494.0')
