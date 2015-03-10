311c4ea9aed99f788f3c5cc4c2072cb30d53cd2c
Doug Cox <dougcoxs1@gmail.com>
2015-03-10 10:49:48 -0700

    Cleaned up Fabric script with a pure python SCP client as paramiko defaults to sftp and the default Yun image does not have that.

--- tools/fabfile.py
+++ tools/fabfile.py
@@ -1,5 +1,7 @@
 from fabric.context_managers import cd
 from fabric.operations import env, local, put, run
+from paramiko import SSHClient
+from scp import SCPClient
 
 def upload_ca():
     # use sh instead of bash
@@ -8,9 +10,15 @@ def upload_ca():
     run('mkdir -p /etc/ssl/certs')
     # install the root ca
     # cannot use 'put' as sftp is not available
-    #put('DigiCertHighAssuranceEVRootCA.pem', '/etc/ssl/certs')
+    ssh = SSHClient()
+    ssh.load_system_host_keys()
     for host in env.hosts:
-        local('scp DigiCertHighAssuranceEVRootCA.pem root@{0}:/etc/ssl/certs/'.format(host))
+        if env.key_filename is None:
+            ssh.connect(host, username=env.user, password=env.password)
+        else:
+             ssh.connect(host)
+        scp = SCPClient(ssh.get_transport())
+        scp.put('DigiCertHighAssuranceEVRootCA.pem', '/etc/ssl/certs') 
     # create the symbolic links normally created by OpenSSL's c_rehash
     with cd('/etc/ssl/certs'):
         run('ln -s DigiCertHighAssuranceEVRootCA.pem 244b5494.0')
